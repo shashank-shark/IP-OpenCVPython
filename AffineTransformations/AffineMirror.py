70268da61b12b5b42ff7f32919984999d7193409
Shashank J <shashankreddy231@gmail.com>
2019-01-28 05:13:56 -0800

    Affine Mirror Transfomation.
    Affine Mirror Transfomation.

---
+++ AffineTransformations/AffineMirror.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+img = cv2.imread ('cat3.jpg', cv2.IMREAD_COLOR)
+
+noOfRows, noOfColumns = img.shape[:2]
+
+# prepare the source points
+src_points = np.float32([[0,0], [noOfColumns-1,0], [0,noOfRows-1]])
+
+# prepare the destination points
+dst_points = np.float32([[noOfColumns-1,0], [0,0], [noOfColumns-1,noOfRows-1]])
+
+# generate the affine matrix
+affine_matrix = cv2.getAffineTransform (src_points, dst_points)
+
+# apply the transform
+img_affineTransform = cv2.warpAffine (img, affine_matrix,(noOfColumns,noOfRows))
+
+# disply the image
+cv2.imshow ('Affine Transform image', img_affineTransform)
+
+cv2.imwrite ('AffineTransformMirror.jpg', img_affineTransform)
+cv2.waitKey(0)
