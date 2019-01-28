44b31759ecfa17710fa2a0ddb9834fa4538d983c
Shashank J <shashankreddy231@gmail.com>
2019-01-28 05:33:33 -0800

    Example of projective Transformation.
    Example of projective Transformation.

---
+++ ProjectiveTransformations/projectiveTransformation.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread ('cat6.jpg', cv2.IMREAD_COLOR)
+rows, cols = img.shape[:2]
+src_points = np.float32([[0,0], [cols-1,0], [0,rows-1], [cols-1,rows-1]])
+dst_points = np.float32([[0,0], [cols-1,0], [int(0.33*cols),rows-1],
+[int(0.66*cols),rows-1]])
+projective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+img_output = cv2.warpPerspective(img, projective_matrix, (cols,rows))
+cv2.imshow('Input', img)
+cv2.imshow('Output', img_output)
+cv2.imwrite('ProjectiveExample.jpg', img_output)
+cv2.waitKey(0)
