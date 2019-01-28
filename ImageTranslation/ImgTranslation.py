f47c25525ad4e7cc4125f27dc9583d98ba6fc0b3
Shashank J <www.shashankreddy@gmail.com>
2019-01-28 03:54:23 -0800

    Update ImgTranslation.py
    Translation of Image using translation of matrix.

--- ImageTranslation/ImgTranslation.py
+++ ImageTranslation/ImgTranslation.py
@@ -1,6 +1,20 @@
 import cv2
 import numpy as np
 
-img = cv2.imread ('cat5.jpg')
+img = cv2.imread ('cat5.jpg', cv2.IMREAD_COLOR)
 
 numOfRows, numOfColumns = img.shape[:2]
+
+# create the translation matrix
+# the translation matrix can be defined as below
+#						[1 0 Tx]
+#	translationMatrix = [0 1 Ty]
+#	here Tx = 70 and Ty = 110 (say)					
+translation_matrix = np.float32 ([[1,0,70],[0,1,110]])
+
+# translation of image
+img_translation = cv2.warpAffine (img, translation_matrix, (numOfColumns, numOfRows), cv2.INTER_LINEAR)
+
+cv2.imshow ('Image Translation', img_translation)
+cv2.waitKey(0)
+
