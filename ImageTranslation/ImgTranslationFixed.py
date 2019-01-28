5fac2a5899f515415ca46d0b13f63cff9dd04c8a
Shashank J <www.shashankreddy@gmail.com>
2019-01-28 04:01:06 -0800

    Create ImgTranslationFixed.py
    Trick for fixing the bound translation.

---
+++ ImageTranslation/ImgTranslationFixed.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+img = cv2.imread ('cat5.jpg', cv2.IMREAD_COLOR)
+
+numOfRows, numOfColumns = img.shape[:2]
+
+# create the translation matrix
+# the translation matrix can be defined as below
+#						[1 0 Tx]
+#	translationMatrix = [0 1 Ty]
+#	here Tx = 70 and Ty = 110 (say)					
+translation_matrix = np.float32 ([[1,0,70],[0,1,110]])
+
+# translation of image
+img_translation = cv2.warpAffine (img, translation_matrix, (numOfColumns+70, numOfRows+110))
+
+cv2.imshow ('Image Translation', img_translation)
+
+# save the image
+cv2.imwrite ('TranslatedImgFixed.jpg', img_translation)
+
+cv2.waitKey(0)
+
