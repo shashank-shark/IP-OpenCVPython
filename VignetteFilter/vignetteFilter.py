8d9f952b35bc5615461e5b43b10220cf4d7d8bd0
Shashank J <shashankreddy231@gmail.com>
2019-01-30 05:07:22 -0800

    Vignette Filter.
    Vignette Filter.

---
+++ VignetteFilter/vignetteFilter.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+img = cv2.imread('bird.jpg')
+rows, cols = img.shape[:2]
+
+# generating vignette mask using Gaussian kernels
+kernel_x = cv2.getGaussianKernel(cols,200)
+kernel_y = cv2.getGaussianKernel(rows,200)
+kernel = kernel_y * kernel_x.T
+mask = 255 * kernel / np.linalg.norm(kernel)
+output = np.copy(img)
+
+# applying the mask to each channel in the input image
+for i in range(3):
+	output[:,:,i] = output[:,:,i] * mask
+
+
+cv2.imshow('Original', img)
+cv2.imshow('VignetteBird', output)
+cv2.imwrite ('VignetteBird.jpg', output)
+cv2.waitKey(0)
