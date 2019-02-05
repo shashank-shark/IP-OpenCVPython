4955d8152840888cba8e8647588128ad09e6f2e5
Shashank J <shashankreddy231@gmail.com>
2019-02-05 04:49:38 -0800

    SIFT
    Scale invariant feature transform.

---
+++ SIFT/sift.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+input_image = cv2.imread('tank.png')
+gray_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+
+# For version opencv < 3.0.0, use cv2.SIFT()
+sift = cv2.xfeatures2d.SIFT_create()
+keypoints = sift.detect(gray_image, None)
+
+cv2.drawKeypoints(input_image, keypoints, input_image, flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+cv2.imshow('SIFT features', input_image)
+cv2.imwrite('SIFT.jpg',input_image)
+cv2.waitKey()
